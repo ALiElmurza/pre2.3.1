cfa20bfac4095c510bc5d4b782994eec5999d7c4
ALiElmurza <ssenioross@gmail.com>
2022-12-26 15:10:12 +0300

    secondThree

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -7,14 +7,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class UsersController {
     @GetMapping()
     public String getUsers(Model model){
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("world");
+        model.addAttribute("messages", list);
 
-
-        return null;
+        return "index";
     }
     @GetMapping("/{id}")
     public String getUserById(@PathVariable("id") int id, Model model){
--- src/main/java/web/dao/DaoImp.java
+++ src/main/java/web/dao/DaoImp.java
@@ -2,13 +2,31 @@ package web.dao;
 
 import web.models.User;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.List;
 
 public class DaoImp implements Dao {
 
+    private static final EntityManagerFactory emFactory;
+
+    static {
+        emFactory = Persistence.createEntityManagerFactory("web.dao");
+    }
+
+    public static EntityManager getEntityManager(){
+        return emFactory.createEntityManager();
+    }
+
+    public static void close(){
+        emFactory.close();
+    }
 
     @Override
     public List<User> getListUsers() {
         return null;
     }
+
+
 }
